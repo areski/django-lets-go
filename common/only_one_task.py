10db7227c95993595d683a30550e4d4181e4efe9
Areski Belaid <areski@gmail.com>
2012-07-25 18:52:51 +0200

    add new decorator for tasks locking

---
+++ common/only_one_task.py
@@ -0,0 +1,31 @@
+import redis
+
+REDIS_CLIENT = redis.Redis()
+
+#code from http://loose-bits.com/2010/10/distributed-task-locking-in-celery.html
+
+
+def only_one(function=None, key="", timeout=None):
+    """Enforce only one celery task at a time."""
+
+    def _dec(run_func):
+        """Decorator."""
+
+        def _caller(*args, **kwargs):
+            """Caller."""
+            ret_value = None
+            have_lock = False
+            lock = REDIS_CLIENT.lock(key, timeout=timeout)
+            try:
+                have_lock = lock.acquire(blocking=False)
+                if have_lock:
+                    ret_value = run_func(*args, **kwargs)
+            finally:
+                if have_lock:
+                    lock.release()
+
+            return ret_value
+
+        return _caller
+
+    return _dec(function) if function is not None else _dec
