b148012573deb40f710d0977049af61d364ed7a5
Areski Belaid <areski@gmail.com>
2012-08-01 11:39:49 +0200

    clean / indent + fix versioning

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 import os
+from common import VERSION
 
 # Compile the list of packages available, because distutils doesn't have
 # an easy way to do this.
@@ -24,10 +25,21 @@ for dirpath, dirnames, filenames in os.walk('common'):
 
 setup(
     name='switch2bill-common',
-    version='0.2',
-    description='Django Common Libs',
+    version="-".join(map(str, VERSION[0:3])) + "".join(VERSION[3:]),
+    description='Common Libs from Star2Billing projects',
     author='Belaid Arezqui',
     author_email='areski@gmail.com',
+    license='MPL 2.0 License',
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers, Users',
+        'License :: OSI Approved :: MPL 2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python, Javascript, HTML',
+        'Topic :: Django Helpers Templates common utils',
+        ],
     packages=packages,
     package_dir={'common': 'common'},
     package_data={'common': data_files},
